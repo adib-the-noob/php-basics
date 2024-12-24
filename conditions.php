37e5f250b2dbe0bad8de00888621fcaacaf9ca12
adib-the-noob <mdadib550@gmail.com>
2024-12-25 01:23:43 +0600

    loops added

---
+++ conditions.php
@@ -0,0 +1,14 @@
+<?php
+
+$a = 200;
+$b = 400;
+$c = 900;
+
+
+if ($a > $b) {
+    echo "a is greater than b";
+} else if ($a < $b) {
+    echo "b is greater than a";
+} else {
+    echo "Ami kisu jani na!";
+}
---
+++ php_constant.php
@@ -0,0 +1,19 @@
+<?php
+
+
+define("ADIB", "2005");
+echo ADIB;
+
+define("cars", [
+  "Audi",
+  "BMW",
+  "Toyota"
+]);
+
+echo "\n";
+
+echo cars[0]; 
+echo "\n";
+echo cars[1];
+
+echo cars[2];
