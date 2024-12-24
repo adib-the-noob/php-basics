c53d9ef52c53ba607d68b7ae084705272da3b039
adib-the-noob <mdadib550@gmail.com>
2024-12-25 01:29:50 +0600

    topic: for loop added

--- switchCases.php
+++ switchCases.php
@@ -1,14 +1,8 @@
-<?php 
+<?php
 
-$i = 0;
-while ($i < 10){
-    echo "$i \n";
-
-    $i++;   
-}
-
-echo "\n==========\n";
-
-for ($i = 0; $i < 20; $i++) {
+// for loops: 
+for ($i = 0; $i < 10; $i++) {
     echo "$i \n";
 }
+
+?>
